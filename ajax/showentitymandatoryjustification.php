6c9f1868320e3b7c716809599e7c9771abd225e6
Thetsmr <thetsmr@gmail.com>
2026-09-23 16:46:42 +0200

    Add checks

--- ajax/showentitymandatoryjustification.php
+++ ajax/showentitymandatoryjustification.php
@@ -43,8 +43,6 @@ Session::checkRight('ticket', UPDATE);
 if (strpos($_SERVER['PHP_SELF'], "showentitymandatoryjustification.php")) {
     header("Content-Type: text/html; charset=UTF-8");
     Html::header_nocache();
-} elseif (!defined('GLPI_ROOT')) {
-    die("Sorry. You can't access this file directly");
 }
 
 if (isset($_POST['entity_selection'])) {
